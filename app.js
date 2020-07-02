648f94f49bef7c1a59eebcd41f83fffa342057d5
jakeschaeffer <j.jakeschaeffer@gmail.com>
2020-07-01 22:58:43 -0700

    got rid of for loops (swapped for .forEach's)

--- app.js
+++ app.js
@@ -21,22 +21,21 @@ var todoList = {
 		var completedTodos = 0;
 
 		// Get number of completed todos
-		for (var i = 0; i < totalTodos; i++) {
-			if (this.todos[i].completed === true) {
+		this.todos.forEach(function(todo) {
+			if (todo.completed === true) {
 				completedTodos++;
 			}
-		}
-
-		// If everything's true, make everything false
-		if (completedTodos === totalTodos) {
-			for (var i = 0; i < totalTodos; i++) {
-				this.todos[i].completed = false;
+		});
+	
+		this.todos.forEach(function(todo) {
+			// Case 1: If everything's true, make everything false.
+			if (completedTodos === totalTodos) {
+				todo.completed = false;
+			// Case 2: Otherwise, make everything true.
+			} else {
+				todo.completed = true;
 			}
-		} else {
-			for (var i = 0; i < totalTodos; i++) {
-				this.todos[i].completed = true;
-			}
-		}
+		});
 	}
 };
 
@@ -75,22 +74,22 @@ var view = {
 	displayTodos: function() {
 		var todosUl = document.querySelector('ul');
 		todosUl.innerHTML = '';
-		for (var i = 0; i < todoList.todos.length; i++) {
+
+		todoList.todos.forEach(function(todo, position) {
 			var todoLi = document.createElement('li');
-			todo = todoList.todos[i];
-			var todoTextWithCompletion = '';
+		 	var todoTextWithCompletion = '';
 
-			if (todo.completed === true) {
-				todoTextWithCompletion = '(x) ' + todo.todoText;
-			} else {
-				todoTextWithCompletion = '( ) ' + todo.todoText;
-			}
+		 	if (todo.completed === true) {
+		 		todoTextWithCompletion = '(x) ' + todo.todoText;
+		 	} else {
+		 		todoTextWithCompletion = '( ) ' + todo.todoText;
+		 	}
 
-			todoLi.id = i;
-			todoLi.textContent = todoTextWithCompletion;
-			todoLi.appendChild(this.createDeleteButton());
-			todosUl.appendChild(todoLi);
-		}
+		 	todoLi.id = position;
+		 	todoLi.textContent = todoTextWithCompletion;
+		 	todoLi.appendChild(this.createDeleteButton());
+		 	todosUl.appendChild(todoLi);
+		}, this);
 	},
 	createDeleteButton: function() {
 		var deleteButton = document.createElement('button');
